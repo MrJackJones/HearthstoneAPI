96b39129716c8fff535002881f3c2ec28facca70
back2 <back2@local.ru>
2022-04-09 21:51:00 +0300

    Initial Commit

---
+++ HearthstoneAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HearthstoneAPI
+//
+//  Created by Ivan on 09.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
